ebdce7b666ac493b72ad1f12689b297a15b7c0bc
Bartłomiej Kulecki <37365394+BartlomiejKulecki@users.noreply.github.com>
2025-10-24 15:10:27 +0200

    Add ros-jazzy-usb-cam to installation requirements

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,3 @@
-sudo apt install -y ros-jazzy-moveit ros-jazzy-moveit-py ros-jazzy-moveit-ros-planning-interface
+sudo apt install -y ros-jazzy-usb-cam ros-jazzy-moveit ros-jazzy-moveit-py ros-jazzy-moveit-ros-planning-interface
 sudo apt install -y python3-opencv
 pip install opencv-contrib-python 'numpy<2' --break-system-packages
